6b097df93abfc2ae67d92090a6999b53b5873fff
Jay5000-bhatt <bhattjay114@gmail.com>
2025-01-29 16:43:51 +0530

    update action.ts

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -63,7 +63,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   } catch (error) {
     // If a database error occurs, return a more specific error.
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      message: 'Database Error: Failed to Create Invoice.', error
     };
   }
 
@@ -96,7 +96,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: 'Database Error: Failed to Update Invoice.', error };
   }
 
   revalidatePath('/dashboard/invoices');
